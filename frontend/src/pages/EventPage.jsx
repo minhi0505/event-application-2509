87cf09f6c299773232f893e3e26220ad529dfaac
minhi0505 <mineehi0505@gmail.com>
2025-09-02 12:27:30 +0900

    [FE] 이벤트 목록 리팩토링

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import EventList from '../components/EventList.jsx';
 
 const EventPage = () => {
 
@@ -8,21 +9,18 @@ const EventPage = () => {
     const fetchEvents = async () => {
         const response = await fetch('http://localhost:9000/api/events');
         const data = await response.json();
+        console.log(data);
 
         setEventList(data);
     };
+
 
     useEffect(() => {
         fetchEvents();
     }, []);
 
     return (
-        <>
-            <h1>Event Page</h1>
-            <ul>
-                {eventList.map(event => <li key={event.eventId}>{event.title}</li>)}
-            </ul>
-        </>
+        <EventList eventList={eventList} />
     );
 };
 
